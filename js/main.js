23bff1a5f4d2a0e5bd2305c14cfad8a1739d0239
kobachiya <kobachiya@gmail.com>
2014-12-08 06:09:00 +0900

    [add]Aboutを追加しました
    * main.js
    MeetupのAPIの書き出しに<ul>（もしくは<ol>）が抜けていたので追加しました。
    * global.css
    PDF当初のフォントと文字色に合わせました。
    * index.html
    Aboutを追加しました。すべての箇所でsectionタグを先頭に配置換えしました。

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 // Main Js Women Who Code
 
 $.getJSON("https://api.meetup.com/2/events?&sign=true&photo-host=public&group_urlname=women-who-code-tokyo&page=4&key=412ff3568274d2345f6c2d13562626&callback=?", function (data){
-	var eventList = "";
+	var eventList = "<ul>";
     $.each(data.results, function (i, item) {
 				var dat1 = new Date(item.time);
 				var weekdays = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
@@ -18,6 +18,7 @@ $.getJSON("https://api.meetup.com/2/events?&sign=true&photo-host=public&group_ur
 				
         eventList += '<li class="grid"><div class="event_date col-3-12">'+year+'/'+(dat1.getMonth() + 1)+'/'+date+'&nbsp;'+weekdays[dat1.getDay()]+'&nbsp;'+hours+':'+minutes+'-'+duraHours+':'+duraMinutes+'</div></div><div class="event_link col-7-12"><a href="' + item.event_url + '" target="_blank">' + item.name  + '</a></div><div class="event_rvsp"><a href="' + item.event_url + '" target="_blank" class="button button_grey">詳細・申し込み</a></div></li>';
     });
+eventList += "</ul>";
     $('#upcoming').html(eventList)});
 
 // "Past Meetups" - The width & the position of the labels on the imgs
